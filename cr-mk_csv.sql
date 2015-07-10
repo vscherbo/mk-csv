a9b1930a105bb1739d62f0950b3311b28e6ebc76
Vladimir Scherbo <vscherbo@gmail.com>
2015-07-10 14:57:29 +0300

    создание csv файла командой copy. Owner postgress

---
+++ cr-mk_csv.sql
@@ -0,0 +1,15 @@
+-- Function: mk_csv(character varying, character varying)
+
+-- DROP FUNCTION mk_csv(character varying, character varying);
+
+CREATE OR REPLACE FUNCTION mk_csv(select_stmt character varying, file_name character varying)
+  RETURNS void AS
+$BODY$
+BEGIN
+  EXECUTE('COPY (' || select_stmt || ') TO ' || QUOTE_LITERAL(file_name) || ' WITH (FORMAT CSV, DELIMITER ''^'', HEADER true )' ); 
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE SECURITY DEFINER
+  COST 100;
+ALTER FUNCTION mk_csv(character varying, character varying)
+  OWNER TO postgres;
