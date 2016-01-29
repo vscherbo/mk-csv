b17448ba962e7bd0565d2dc530c3288e7869d61e
Vladimir Scherbo <vscherbo@gmail.com>
2016-01-29 14:00:11 +0300

    файл создаётся в каталоге ~/mk_csv_data

--- cr-mk-csv-general.sql
+++ cr-mk-csv-general.sql
@@ -91,7 +91,7 @@ BEGIN
 
 
   IF flg_single THEN 
-     out_csv = '/tmp/' || ib_name || '.csv';
+     out_csv = '/var/lib/pgsql/mk_csv_data/' || ib_name || '.csv';
      -- SELECT * INTO debug_rec FROM csv_tmp;
      -- RAISE NOTICE 'csv_tmp=%', debug_rec ;
      SELECT mk_csv('SELECT * FROM ' ||csv_tmp_name, out_csv) INTO out_res;
