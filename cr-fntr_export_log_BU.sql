d430c6216be21c4e6f07fd5616c94cd5f2a83fc9
Vladimir Scherbo <vscherbo@gmail.com>
2016-02-25 15:58:25 +0300

    разрешено в журнаде экспорта менять признак текущей версии на архивную

--- cr-fntr_export_log_BU.sql
+++ cr-fntr_export_log_BU.sql
@@ -11,15 +11,22 @@ BEGIN
   -- экспорт на боевой сайт завершён успешно
   IF (OLD.exp_mod = OLD.exp_csv_status) AND (OLD.exp_site = 'kipspb.ru') AND (OLD.exp_status >= 1)
   THEN 
-     RAISE E'Запрещено редактировать историю синхронизации.';
+     IF (OLD.exp_status = 1 ) AND (NEW.exp_version_num > 1 )
+     THEN
+        -- TODO проверить, что изменяется только NEW.exp_version_num
+        -- или вынести в отдельный триггер по полю exp_version_num
+        NULL; -- разрешено завершённый экспорт переводить в архив
+     ELSE
+        RAISE E'Запрещено редактировать архив синхронизации.';
+     END IF;
   ELSE
     SELECT * INTO dev FROM devmod.device WHERE dev_id = NEW.dev_id AND version_num = NEW.exp_version_num;
     IF NOT FOUND THEN 
        RAISE E'Не найден прибор dev_id=%, version_num=%.', NEW.dev_id, NEW.exp_version_num;
     ELSIF dev.ie_xml_id IS NULL AND NEW.exp_mod IN (1, 7) THEN
-        NULL; -- permited combination, just pass
+        NULL; -- разрешённая комбинация, идём дальше 
     ELSIF dev.ie_xml_id IS NOT NULL AND NEW.exp_mod IN (1, 4, 6, 7) THEN
-        NULL; -- permited combination, just pass
+        NULL; -- разрешённая комбинация, идём дальше
     ELSE
        RAISE E'Этот режим экспорта не реализован ie_xml_id=%, exp_mod=%.', dev.ie_xml_id, NEW.exp_mod;
     END IF; -- NOT FOUND
