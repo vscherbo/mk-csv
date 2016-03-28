b352b5fd1079166a5600abe97959471c54370284
Vladimir Scherbo <vscherbo@gmail.com>
2016-03-28 15:10:54 +0300

    добавлена обработка сигнала do_single

--- pg-listener/pg-listener.py
+++ pg-listener/pg-listener.py
@@ -13,7 +13,8 @@ import psycopg2
 import psycopg2.extensions
 import logging
 
-pg_channel = 'do_export'
+#pg_channel = 'do_export'
+pg_channels = ('do_export', 'do_single')
 pg_timeout = 5
 mark_display=3600
 
@@ -33,8 +34,6 @@ SIGNALS_TO_NAMES_DICT = dict((getattr(signal, n), n) for n in dir(signal)
 
 
 def signal_handler(asignal, frame):
-    # print('\nGot signal: ',
-    #      SIGNALS_TO_NAMES_DICT.get(asignal, "Unnamed signal: %d" % asignal))
     logging.info('Got signal: %s',
           SIGNALS_TO_NAMES_DICT.get(asignal, "Unnamed signal: %d" % asignal))
     global do_while
@@ -47,43 +46,83 @@ signal.signal(signal.SIGHUP, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 
 
+def do_mk_csv(notify):
+    try:
+        curs.callproc('devmod.fn_mk_csv', [notify.payload])
+    except psycopg2.Error, exc:
+        logging.warning("%s _exception_fn_mk_csv=%s", parser.prog, str(exc))
+        curs.execute('SELECT exp_creator FROM devmod.bx_export_log\
+                     WHERE exp_id='
+                     + notify.payload)
+        emp_id = curs.fetchone()
+        try:
+            curs.callproc('fn_push_article2user', [emp_id, 'exp_id='+ notify.payload + '/' + str(exc)])
+        except psycopg2.Error, exc:
+            logging.warning("%s _excecption_fn_push_article2user=%s", parser.prog, str(exc))
+
+        try:
+            curs.execute("UPDATE devmod.bx_export_log SET exp_result = quote_literal('" + str(exc).replace("'", "''") +  "') WHERE exp_id = " + notify.payload + ";" )
+        except psycopg2.Error, exc:
+            logging.warning("%s _excecption_UPDATE bx_export_log=%", parser.prog, str(exc))
+    logging.info("Finish fn_mk_csv")
+
+def do_set_single(notify):
+    logging.info("Inside do_set_single")
+    time_delivery = u''
+    (str_modid, stock_status) = notify.payload.split('^')
+    if '0' == stock_status: # NOT in stock
+        logging.info("not_in_stock branch")
+        curs.callproc('devmod.get_def_time_delivery', [str_modid])
+        res = curs.fetchone()
+        time_delivery = res[0]
+        logging.info("type of time_delivery=%s", type(time_delivery))
+        logging.info("got default time_delivery=%s", time_delivery)
+        #if time_delivery.find('not found') > 0:
+        #    print 'ERROR. found'
+        #else:
+        #    print 'OK, not found'
+        if time_delivery.find('not found') > 0:
+            logging.warning("time_delivery = None")
+            time_delivery = None # time_delivery is incorrect
+        else:
+            logging.info("time_delivery = %s", time_delivery)
+    elif '1' == stock_status:
+        time_delivery = u'Ожидается на склад'
+        logging.info("time_delivery = %s", time_delivery)
+    elif '2' == stock_status:
+        time_delivery = u'Со склада'
+        logging.info("time_delivery = %s", time_delivery)
+    else:   
+        # TODO wrong stock_status
+        time_delivery = None
+        logging.warning("wrong stock_status={%s}, time_delivery = None", stock_status)
+
+    if time_delivery != None:
+        # TODO - choose site
+        site = 'kipspb-fl.arc.world'
+        logging.info("before call devmod.set_mod_timedelivery([%s], [%s], [%s])", site, str_modid, time_delivery)
+        curs.callproc('devmod.set_mod_timedelivery', [site, str_modid, time_delivery])
+    else:
+        logging.info("? time_delivery == None")
+    logging.info("Finish set_mod_timedelivery")
+
 def do_listen(a_pg_timeout):
     sel_res = select.select([conn], [], [], a_pg_timeout)
     if sel_res == ([], [], []):
         pass
-        # print("%s Timeout" % datetime.now())
         logging.debug("Timeout %s sec", a_pg_timeout)
     else:
         conn.poll()
-        # while (1 == do_while) and conn.notifies:
         while conn.notifies:
             notify = conn.notifies.pop()
             logging.info(" Got NOTIFY: %s %s %s", notify.pid, notify.channel, notify.payload)
-            # print(str(datetime.now()) + " Got NOTIFY:",
-            #      notify.pid, notify.channel, notify.payload)
-            try:
-                curs.callproc('devmod.fn_mk_csv', [notify.payload])
-            except psycopg2.Error, exc:
-                # print("% _exc_fn_mk_csv=%", parser.prog, exc)
-                logging.warning("%s _exception_fn_mk_csv=%s", parser.prog, str(exc))
-                curs.execute('SELECT exp_creator FROM devmod.bx_export_log\
-                             WHERE exp_id='
-                             + notify.payload)
-                emp_id = curs.fetchone()
-                try:
-                    curs.callproc('fn_push_article2user', [emp_id, 'exp_id='+ notify.payload + '/' + str(exc)])
-                except psycopg2.Error, exc:
-                    logging.warning("%s _excecption_fn_push_article2user=%s", parser.prog, str(exc))
-                    # print("% _exc_fn_push_article2user=%", parser.prog, exc)
-
-                try:
-                    curs.execute("UPDATE devmod.bx_export_log SET exp_result = quote_literal('" + str(exc).replace("'", "''") +  "') WHERE exp_id = " + notify.payload + ";" )
-                except psycopg2.Error, exc:
-                    logging.warning("%s _excecption_UPDATE bx_export_log=%", parser.prog, str(exc))
-                    # print("% _exc UPDATE bx_export_log=%", parser.prog, exc)
-            conn.commit()
-            logging.info("Finish fn_mk_csv")
-            # print(str(datetime.now()) + " Finish fn_mk_csv")
+            if 'do_export' == notify.channel:
+                do_mk_csv(notify)
+            elif 'do_single' == notify.channel:
+                do_set_single(notify)
+            else:
+                pass
+        conn.commit()
 # End of do_listen
 
 
@@ -95,7 +134,6 @@ if not isinstance(numeric_level, int):
     raise ValueError('Invalid log level: %s' % loglevel)
 logging.basicConfig(filename='pg-listener.log', format='%(asctime)s %(levelname)s: %(message)s', level=numeric_level) # INFO)
 
-# print("Started at %s" % datetime.now())
 logging.info("Started")
 do_connect = 1
 while 1 == do_connect:
@@ -105,14 +143,13 @@ while 1 == do_connect:
         conn = psycopg2.connect(DSN)
         conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
         curs = conn.cursor()
-        curs.execute("LISTEN " + pg_channel + ";")
+        for pg_channel in pg_channels:
+            curs.execute("LISTEN " + pg_channel + ";")
+            logging.info("Waiting for notifications on channel %s", pg_channel)
         mark_counter=0
-        # print("Waiting for notifications on channel %s" % pg_channel)
-        logging.info("Waiting for notifications on channel %s", pg_channel)
         do_while = 1
     except BaseException, exc:
         logging.warning(" Exception on connect=%s. Sleep for %s", str(exc), str(pg_timeout))
-        # print(" Exception on connect=", exc.args)
         do_while = 0
         sleep(pg_timeout);
 
@@ -124,21 +161,16 @@ while 1 == do_connect:
             if mark_counter >= mark_display:
                 mark_counter=0
                 logging.info("Heartbeat mark")
-                # print("%s Heartbeat mark" % datetime.now())
         except BaseException, exc:
             if 4 == exc.args[0]:  # interrupt
                 # do_while = 0
                 rc = 0
-                # print(" exception4=", exc.args[1])
                 logging.info("exception4, %s", exc.args[1])
             elif exc.args[0].find('closed'):
                 do_while = 0
                 logging.info("Try to re-connect...")
-                # print("Try to re-connect...")
             else:
                 logging.warning("Other exception=%s", str(exc))
-                # print(" Other exception=", exc.args)
 
-# print(str(datetime.now()) + " Exiting...")
 logging.info("Exiting")
 exit(rc)
