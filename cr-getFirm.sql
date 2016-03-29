fd984777f8059578ec5c3fd06231c30dd7585692
Vladimir Scherbo <vscherbo@gmail.com>
2016-03-29 14:28:20 +0300

    по таблице ВВ

--- cr-getFirm.sql
+++ cr-getFirm.sql
@@ -1,31 +1,45 @@
--- Function: "getFirm"(integer)
+-- Function: "getFirm"(integer, boolean)
 
--- DROP FUNCTION "getFirm"(integer);
+-- DROP FUNCTION "getFirm"(integer, boolean);
 
-CREATE OR REPLACE FUNCTION "getFirm"(acode integer)
+CREATE OR REPLACE FUNCTION "getFirm"(
+    acode integer,
+    flgowen boolean)
   RETURNS character varying AS
 $BODY$
 DECLARE
   ourFirm VARCHAR;
   flgDealer BOOLEAN;
+  lastFirm VARCHAR;
 BEGIN
-/**/
+
 IF "КодПредприятия" = 223719 THEN
-   ourFirm = 'АРКОМ';
-   RETURN ourFirm;
+    ourFirm = 'АРКОМ';
+ELSE 
+    select INTO flgDealer exists(select 1 from vwДилеры WHERE "Код"= aCode);
+    IF flgDealer THEN
+        ourFirm = 'КИПСПБ';
+    ELSIF flgOwen THEN
+        ourFirm = 'ОСЗ';
+    ELSE
+        SELECT "фирма" INTO lastFirm FROM "Счета" WHERE "Код" = aCode AND "Дата счета" IS NOT NULL ORDER BY "Дата счета" DESC LIMIT 1;
+        IF FOUND THEN
+            ourFirm := lastFirm; -- see Patch below
+        ELSE
+            ourFirm = 'ЭТК';
+        END IF; -- FOUND
+    END IF;
+
 END IF;
-   
-select INTO flgDealer exists(select 1 from vwДилеры WHERE "Код"= aCode);
-IF flgDealer THEN
-   ourFirm = 'КИПСПБ';
-ELSE
-   ourFirm = 'ЭТК';
-END IF;      
-/**/
+
+IF 'ТД2' == ourFirm THEN -- Patch
+    ourFirm := 'ЭТК';
+END IF;
+
 RETURN ourFirm;
 END;$BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-ALTER FUNCTION "getFirm"(integer)
+ALTER FUNCTION "getFirm"(integer, boolean)
   OWNER TO arc_energo;
-COMMENT ON FUNCTION "getFirm"(integer) IS 'Возвращает аббревиатуру нашей компании, от которой будет сформирован счёт';
+COMMENT ON FUNCTION "getFirm"(integer, boolean) IS 'Возвращает аббревиатуру нашей компании, от которой будет сформирован счёт';
