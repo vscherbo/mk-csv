d1a44c3b03f090e69d4cd1baf1cf77d4c3c2a11a
Vladimir Scherbo <vscherbo@gmail.com>
2016-10-06 17:32:08 +0300

    оформление pgAdmin

--- cr-get_ks.sql
+++ cr-get_ks.sql
@@ -1,4 +1,8 @@
-CREATE OR REPLACE FUNCTION get_ks(arg_mod_id character varying )
+-- Function: get_ks(character varying)
+
+-- DROP FUNCTION get_ks(character varying);
+
+CREATE OR REPLACE FUNCTION get_ks(arg_mod_id character varying)
   RETURNS integer AS
 $BODY$SELECT "КодСодержания" AS result 
 FROM devmod.modifications
@@ -6,4 +10,6 @@ WHERE mod_id = arg_mod_id AND version_num = 1;
 $BODY$
   LANGUAGE sql VOLATILE
   COST 100;
+ALTER FUNCTION get_ks(character varying)
+  OWNER TO arc_energo;
 COMMENT ON FUNCTION get_ks(character varying) IS 'Возвращает КодСодержания, найденный  по идентификатору модификации';
