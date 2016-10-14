e02bfd0e3340204fe33d682282773b3f4dc1f577
Vladimir Scherbo <vscherbo@gmail.com>
2016-10-14 10:08:44 +0300

    защита от несинхронизированных КС

--- cr-fntr-expected-shipments-notify.sql
+++ cr-fntr-expected-shipments-notify.sql
@@ -5,9 +5,16 @@
 CREATE OR REPLACE FUNCTION fntr_expected_shipments_notify()
   RETURNS trigger AS
 $BODY$
+DECLARE
+  loc_mod_id INTEGER;
 BEGIN
-  EXECUTE pg_notify('do_expected', get_mod_id(NEW.ks) || '^' || NEW.expected || '^' || NEW.id::VARCHAR);
-  RETURN NEW;
+    loc_mod_id := get_mod_id(NEW.ks);
+    IF loc_mod_id IS NOT NULL THEN
+        EXECUTE pg_notify('do_expected', loc_mod_id || '^' || NEW.expected || '^' || NEW.id::VARCHAR);
+    ELSE
+        RAISE NOTICE 'mod_id for KS=% not found', NEW.ks;
+    END IF;
+    RETURN NEW;
 END;$BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
