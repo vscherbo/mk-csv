40e1718eb2e2b8ada8f47b61dd1cd4823b78450d
Vladimir Scherbo <vscherbo@gmail.com>
2017-03-30 11:05:45 +0300

    обработка ошибок; обновление товара по результатам импорта через симв. ссылку на файл csv

--- cr-mk_csv_prices_batch.sql
+++ cr-mk_csv_prices_batch.sql
@@ -12,10 +12,14 @@ $BODY$DECLARE
   price_mode bit(3) = B'100';
   exp RECORD;
   upd_str VARCHAR;
-  loc_exp_result VARCHAR;
-  loc_exp_status INTEGER := 1; -- успешно, если будут ошибки, сбрасываем в 0
+  loc_exp_result VARCHAR := '';
+  loc_exp_status INTEGER := 1; -- успешно, если будут ошибки, устанавливаем в -1
   loc_batch_result VARCHAR := 'Были ошибки в пакетном экспорте';
   csv_filename VARCHAR;
+  loc_who INTEGER;
+  loc_title VARCHAR;
+  loc_dt_created TIMESTAMP WITHOUT TIME ZONE;
+  loc_article VARCHAR;
 BEGIN
   -- check every exp has 
      -- already synced (not NULL device 's fileds: ie_xml_id, ip_prop674, ip_prop 675)
@@ -26,6 +30,10 @@ BEGIN
           FROM devmod.bx_export_log 
           WHERE exp_batch_id=abatch_id
           AND exp_status = 0; -- только не выгруженные 
+
+  IF NOT FOUND THEN
+     RETURN;
+  END IF;
 
   SELECT exp_site into site 
          FROM devmod.bx_export_log 
@@ -53,36 +61,47 @@ BEGIN
  /***/
  res_exec := public.exec_paramiko(site, 22, 'uploader'::VARCHAR, cmd);
  IF res_exec.err_str <> '' THEN 
-    loc_exp_status := 0;
+    loc_exp_status := -1;
     loc_exp_result := res_exec.err_str;
-    RAISE 'Import prices cmd=%^err_str=[%]', cmd, res_exec.err_str; 
+    RAISE NOTICE 'Import prices cmd=%^err_str=[%]', cmd, res_exec.err_str; 
  END IF;
  IF res_exec.out_str <> '' THEN 
-    loc_exp_status := 0;
+    loc_exp_status := -1;
     loc_exp_result := loc_exp_result || '/' || res_exec.out_str;
-    RAISE 'Import prices cmd=%^out_str=[%]', cmd, res_exec.out_str; 
+    RAISE NOTICE 'Import prices cmd=%^out_str=[%]', cmd, res_exec.out_str; 
  END IF;
 
- -- update fin info
- cmd := '/usr/bin/php $ARC_PATH/fin-info-update-list.php -f upload/import-update.csv';
+ -- update fin info. file fin-info-update-list.csv is symlink to the just imported csv (see run-import-profile.sh)
+ cmd := '/usr/bin/php $ARC_PATH/fin-info-update-list.php -f /home/uploader/upload/fin-info-update-list.csv ';
  res_exec := public.exec_paramiko(site, 22, 'uploader'::VARCHAR, cmd);
  IF res_exec.err_str <> '' THEN
-    loc_exp_status := 0;
+    loc_exp_status := -1;
     loc_exp_result := loc_exp_result || '/' || res_exec.err_str;
-    RAISE 'fin-info-update-list cmd=%^err_str=[%]', cmd, res_exec.err_str; 
+    RAISE NOTICE 'fin-info-update-list cmd=%^err_str=[%]', cmd, res_exec.err_str; 
  END IF;
 /***/
 
+
 /** TODO **/
-IF loc_exp_result IS NULL
+IF '' = loc_exp_result 
 THEN
     loc_exp_result := quote_literal('Завершён экспорт в составе пакета batch_id=' || abatch_id);
     loc_batch_result := quote_literal('Завершён пакетный экспорт');
+ELSE
+    loc_exp_result := quote_literal('Ошибка экспорта в составе пакета batch_id=' || abatch_id);
+    loc_batch_result := quote_literal('Ошибка пакетного экспорта');
+    SELECT who, title, dt_created INTO loc_who, loc_title, loc_dt_created FROM bx_export_bat WHERE id=abatch_id;
+    loc_article := format('Пакетный экспорт №%s %s от %s завершился с ошибкой:%s',
+                           abatch_id, quote_literal(loc_title), loc_dt_created, loc_exp_result);
+    PERFORM push_arc_article(loc_who, loc_article, 1);
+    IF loc_who <> 124 THEN
+        PERFORM push_arc_article(124, loc_article, 1); -- ВН
+    END IF;
 END IF;
 
 FOR exp IN SELECT exp_id, dev_id, exp_mod, exp_version_num FROM devmod.bx_export_log WHERE exp_batch_id=abatch_id
 LOOP
-    upd_str := 'ie_xml_id_dt = now(), ip_prop674_dt = now()' ;
+    upd_str := 'ie_xml_id_dt = clock_timestamp(), ip_prop674_dt = clock_timestamp()' ;
     upd_str := 'UPDATE devmod.device SET ' || upd_str || 
                ' WHERE dev_id = ' || exp.dev_id || ' AND version_num = ' || exp.exp_version_num || ';';
     RAISE NOTICE 'UPDATE device=%', upd_str;
