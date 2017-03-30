e64558c622b050f96248db622778613d4cc7aecb
Vladimir Scherbo <vscherbo@gmail.com>
2017-03-30 11:08:31 +0300

    формирование payload для notify через format

--- cr-fntr-single-notify.sql
+++ cr-fntr-single-notify.sql
@@ -6,8 +6,8 @@ CREATE OR REPLACE FUNCTION fntr_single_notify()
   RETURNS trigger AS
 $BODY$
 BEGIN
-  -- EXECUTE pg_notify('do_single', NEW.mod_id || '^' || NEW.stock_status_new::VARCHAR || '^' || NEW.id::VARCHAR);
-  EXECUTE pg_notify('do_single', NEW.mod_id || '^' || NEW.time_delivery::VARCHAR || '^' || NEW.id::VARCHAR || '^' || COALESCE(NEW.qnt::VARCHAR, ''));
+  -- EXECUTE pg_notify('do_single', NEW.mod_id || '^' || NEW.time_delivery::VARCHAR || '^' || NEW.id::VARCHAR || '^' || COALESCE(NEW.qnt::VARCHAR, ''));
+  EXECUTE pg_notify('do_single', format('%s^%s^%s^%s',NEW.mod_id, NEW.time_delivery, NEW.id, NEW.qnt));
   RETURN NEW;
 END;$BODY$
   LANGUAGE plpgsql VOLATILE
