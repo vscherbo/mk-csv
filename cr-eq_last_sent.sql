268abbeb7f8629de61206765141041c097609c00
Vladimir Scherbo <vscherbo@gmail.com>
2017-04-06 13:19:52 +0300

    проверка на дубль перед добавлением в stock_status_changed

---
+++ cr-eq_last_sent.sql
@@ -0,0 +1,40 @@
+-- Function: eq_last_sent(character varying, character varying, numeric)
+
+-- DROP FUNCTION eq_last_sent(character varying, character varying, numeric);
+
+CREATE OR REPLACE FUNCTION eq_last_sent(
+    a_mod_id character varying,
+    a_time_delivery character varying,
+    a_qnt numeric)
+  RETURNS boolean AS
+$BODY$
+DECLARE
+loc_time_delivery VARCHAR;
+loc_qnt NUMERIC;
+loc_equal BOOLEAN default false;
+loc_id INTEGER;
+loc_cnt INTEGER;
+
+
+BEGIN
+SELECT time_delivery, qnt, id, dbl_cnt INTO loc_time_delivery, loc_qnt, loc_id, loc_cnt 
+FROM arc_energo.stock_status_changed
+WHERE mod_id=a_mod_id
+    AND change_status=1
+ORDER BY dt_sent DESC LIMIT 1;
+
+loc_equal:= (FOUND AND (loc_qnt=a_qnt) AND (loc_time_delivery=a_time_delivery));
+
+IF loc_equal THEN
+    UPDATE stock_status_changed SET dbl_cnt = (loc_cnt + 1) WHERE id=loc_id; 
+END IF;
+
+RETURN loc_equal;
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION eq_last_sent(character varying, character varying, numeric)
+  OWNER TO arc_energo;
+
