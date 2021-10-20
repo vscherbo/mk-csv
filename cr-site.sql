fc7a9b3d7bc792c74def6f6e9f504a0f2a701646
Vladimir Scherbo <vscherbo@gmail.com>
2021-10-20 13:15:24 +0300

    arc_const devsite

--- cr-site.sql
+++ cr-site.sql
@@ -9,7 +9,8 @@ loc_production := pg_production();
 IF loc_production THEN
   RETURN 'kipspb.ru';
 ELSE
-  RETURN 'kipspb-fl.arc.world';
+  -- RETURN 'kipspb-fl.arc.world';
+  RETURN arc_const('devsite');
 END IF;
 
 END;$BODY$
