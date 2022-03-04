04c64d1c5c077fe93f1d8c93902b6afe5ebb082d
Vladimir Scherbo <vscherbo@gmail.com>
2022-03-04 09:35:27 +0300

    no exceptions

--- pg-listener/update_site.py
+++ pg-listener/update_site.py
@@ -16,10 +16,11 @@ def do_update_site(site, cmd):
     logging.info(ret_str)
 
     try:
-        (out_str, err_str) = exec_paramiko.exec_paramiko(site, 'uploader', cmd)  # PRODUCTION
+        (out_str, err_str) = exec_paramiko.exec_paramiko(site, 'uploader', cmd, timeout=15)  # PRODUCTION
+        # (out_str, err_str) = exec_paramiko.exec_paramiko(site, 'uploader', cmd, timeout=5, port=2222)  # DEBUG timeout
     except BaseException, exc:
         ret_str = 'do_update_site exec_paramiko exception={0}'.format(exc)
-        logging.exception(ret_str, exc_info=True)
+        #logging.exception(ret_str, exc_info=True)
         #raise
     else:
         ret_str = out_str
@@ -28,19 +29,38 @@ def do_update_site(site, cmd):
     return ret_str
 
 
-def set_mod_timedelivery(site, mod_code, mod_timedelivery, mod_qnt=''):
-    cmd = "php $ARC_PATH/update-single-modification.php -m{0} -t'{1}' -q'{2}'".format(mod_code,\
-            mod_timedelivery, mod_qnt)
+def set_mod_timedelivery(site, mod_code, mod_timedelivery, mod_qnt = '', active=True):
+    cmd = "php $ARC_PATH/update-single-modification.php -m{0} -t'{1}' -q'{2}'".format(mod_code, mod_timedelivery, mod_qnt)
+    logging.info('set_mod_timedelivery, active=%s', active);
+    if active is None:  # PATCH!!!
+        active = True
+
+    if not active:
+        logging.info('set_mod_timedelivery, add -aN');
+        cmd = "{0} -aN".format(cmd)
+
+    rest_str = None
     try:
         ret_str = do_update_site(site, cmd)
     except Exception, exc:
-        ret_str = 'set_mod_timedelivery call do_update_site exception={0}'.format(exc)
+        ret_str = 'set_mod_timedelivery call do_update_site exception={0}'.format(ret_str)
+        #raise
+    if ret_str and ret_str != '':
+        raise ValueError(ret_str) 
     return ret_str
 
 def set_mod_expected_shipments(site, mod_code, mod_expected_shipments):
     cmd = "php $ARC_PATH/update-expected-shipments.php -m{0} -e'{1}'".format(mod_code,\
             mod_expected_shipments)
-    return do_update_site(site, cmd)
+    try:
+        ret_str = do_update_site(site, cmd)
+    except Exception, exc:
+        ret_str = 'set_mod_expected_shipments call do_update_site exception={0}'.format(ret_str)
+        #raise
+    if ret_str and ret_str != '':
+        raise ValueError(ret_str) 
+    return ret_str
+    #return do_update_site(site, cmd)
 
 
 if __name__ == "__main__":
@@ -50,6 +70,6 @@ if __name__ == "__main__":
     logging.basicConfig(filename=PRG_NAME+'.log', format=LOG_FORMAT, level=logging.INFO)
 
     logging.info('Started')
-    # set_mod_timedelivery('kipspb-fl.arc.world', '010620000004', 'Со склада', 13)
+    # set_mod_timedelivery('kipspb-dev.arc.world', '010620000004', 'Со склада', 13)
     set_mod_expected_shipments('kipspb-dev.arc.world', '010620000004', '2017-12-12:12;')
     logging.info('Finished')
