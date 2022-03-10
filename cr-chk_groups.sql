725d41e170aee2f1e8464f113d591084f0d2e1f6
Vladimir Scherbo <vscherbo@gmail.com>
2022-03-10 10:17:23 +0300

    флаг inactive для уценки

--- cr-chk_groups.sql
+++ cr-chk_groups.sql
@@ -6,7 +6,8 @@ CREATE OR REPLACE FUNCTION devmod.chk_groups(
     devicename character varying,
     site character varying,
     username character varying DEFAULT 'uploader'::character varying,
-    port integer DEFAULT 22)
+    port integer DEFAULT 22,
+    inactive boolean DEFAULT 'False')
   RETURNS record AS
 $BODY$
 DECLARE
@@ -15,13 +16,16 @@ DECLARE
   out_str varchar;
   res RECORD;
 BEGIN
-   devicename := quote_literal(devicename);
-   cmd := 'php -f $ARC_PATH/chk-groups.php ' || devicename;
-   res := public.exec_paramiko(site, 22, 'uploader', cmd);
-   RETURN res;
+    devicename := quote_literal(devicename);
+    cmd := 'php -f $ARC_PATH/chk-groups.php ' || devicename;
+    if inactive then
+        cmd := cmd || ' inactive';
+    end if;
+    res := public.exec_paramiko(site, port, username, cmd);
+    RETURN res;
 END;$BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100;
+    LANGUAGE plpgsql VOLATILE
+    COST 100;
 ALTER FUNCTION devmod.chk_groups(character varying, character varying, character varying, integer)
-  OWNER TO arc_energo;
+    OWNER TO arc_energo;
 COMMENT ON FUNCTION devmod.chk_groups(character varying, character varying, character varying, integer) IS 'Возвращает ветки каталога, в которых найден товар с заданным именем';
