fa885bc6f840cb7d7b45ed67e9c6a028996000e7
Vladimir Scherbo <vscherbo@gmail.com>
2022-03-25 15:04:34 +0300

    move from dbl prec to NUMERIC

--- queries-reports/sync-errors.sql
+++ queries-reports/sync-errors.sql
@@ -21,6 +21,7 @@ SELECT * FROM vrf_sync_site_failure();
 
 \echo 'Расхождения в ценах'
 \echo
+
 SELECT 
 	t1.КодСодержания, 
 	-- t1.mod_id, 
@@ -33,9 +34,9 @@ FROM
 	(SELECT Содержание.КодСодержания, 
 		modifications.mod_id, 
 		dev_name ||': '|| mod_id AS modname, 
-		Содержание.Цена AS "Цена(сод.)", 
-		vwЦены.ОтпускнаяЦена AS "Цена(Цены)", 
-		modifications.mod_price AS "Цена(devmod)",
+		Содержание.Цена::NUMERIC AS "Цена(сод.)", 
+		vwЦены.ОтпускнаяЦена::NUMERIC AS "Цена(Цены)", 
+		modifications.mod_price::NUMERIC AS "Цена(devmod)",
 		modifications.dev_id
        FROM arc_energo.Содержание
        JOIN arc_energo.vwЦены ON Содержание.КодСодержания = vwЦены.КодСодержания
@@ -44,7 +45,7 @@ FROM
 	WHERE modifications.version_num=1 AND device.version_num=1
 	AND Not device.ie_xml_id Is Null) AS t1
 LEFT JOIN devmod.bx_price ON t1.modname = bx_price.ie_name
-WHERE NOT (t1."Цена(сод.)" = t1."Цена(Цены)" AND t1."Цена(сод.)" =t1."Цена(devmod)" AND t1."Цена(сод.)" =cv_price)
+WHERE NOT (round(t1."Цена(сод.)"::NUMERIC,2) = round(t1."Цена(Цены)"::numeric,2) AND round(t1."Цена(сод.)"::NUMERIC,2) =round(t1."Цена(devmod)"::NUMERIC,2) AND round(t1."Цена(сод.)"::NUMERIC,2) =round(cv_price::numeric,2))
 AND t1.dev_id Not IN (SELECT d.dev_id
        FROM devmod.bx_dev b JOIN devmod.device d ON b.ie_xml_id = d.ie_xml_id
        WHERE d.version_num = 1 AND NOT b.ie_active);
