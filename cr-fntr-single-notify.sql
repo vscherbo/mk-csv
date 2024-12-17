87c3ee31225dd685f221fe047efff7e34254a64d
Владимир Щербо <vscherbo@gmail.com>
2024-12-17 14:00:00 +0300

    do_compute_single only

--- cr-fntr-single-notify.sql
+++ cr-fntr-single-notify.sql
@@ -1,19 +1,14 @@
--- Function: fntr_single_notify()
+-- DROP FUNCTION arc_energo.fntr_single_notify();
 
--- DROP FUNCTION fntr_single_notify();
-
-CREATE OR REPLACE FUNCTION fntr_single_notify()
-  RETURNS trigger AS
-$BODY$
+CREATE OR REPLACE FUNCTION arc_energo.fntr_single_notify()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
 BEGIN
-  if sync_is_new() then
+
       EXECUTE pg_notify('do_compute_single', format('%s',NEW.id));
-  else
-      EXECUTE pg_notify('do_single', format('%s^%s^%s^%s',NEW.mod_id, NEW.time_delivery, NEW.id, NEW.qnt));
-  end if;
+
   RETURN NEW;
-END;$BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100;
-ALTER FUNCTION fntr_single_notify()
-  OWNER TO arc_energo;
+END;$function$
+;
+
