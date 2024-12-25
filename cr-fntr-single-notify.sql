b78b07ad015ec0e1fb05c056fc42ad45e7bb68ae
Владимир Щербо <vscherbo@gmail.com>
2024-12-25 13:23:39 +0300

    NO Ozon actions in this function

--- cr-fntr-single-notify.sql
+++ cr-fntr-single-notify.sql
@@ -9,17 +9,6 @@ loc_calc NUMERIC;
 loc_id integer;
 BEGIN
     EXECUTE pg_notify('do_compute_single', format('%s',NEW.id));
-    IF NEW.ks IS NOT NULL THEN
-        PERFORM FROM ext.ozon_list WHERE NEW.ks = ks;
-        IF FOUND THEN
-            loc_calc := stock_pack_availability(NEW.ks);
-            WITH inserted AS (INSERT INTO ozon_stock_changed(ssc_id, ks, qnt_ssc, qnt_calc)
-                              VALUES (NEW.id, NEW.ks, NEW.qnt, loc_calc)
-                              RETURNING id)
-            SELECT id INTO loc_id FROM inserted;
-            EXECUTE pg_notify('do_ozon_item', format('%s^%s^%s', NEW.ks, loc_calc, loc_id));
-        END IF;    
-    END IF;    
     RETURN NEW;
 END;$function$
 ;
